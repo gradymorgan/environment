7484fc4ccb12d31c950ddd836fb50f4f7f96e3ff
Grady Morgan <gradymorgan@gmail.com>
2016-02-04 20:19:57 -0800

    add sensor initialization and error handling

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ var BMP280 = require('node-bmp280');
 
 exports.load = function(server, boatData, settings) {
 	var barometer = new BMP280();
-
-    setInterval(function() {
-        barometer.readPressureAndTemparature(function(err, pressure, temperature) {
-            boatData.broadcast({
-                type: 'data',
-                subtype: 'env',
-                values: [
-                	"P",
-                	pressure,
-                	"T", 
-                	temperature
-                ]
+    
+    barometer.begin(function(err) {
+        if (err) {
+            console.error('unable to initialize barometer', err);
+            return;
+        }
+        setInterval(function() {
+            barometer.readPressureAndTemparature(function(err, pressure, temperature) {
+                boatData.broadcast({
+                    type: 'data',
+                    subtype: 'env',
+                    values: [
+                        "P",
+                        pressure,
+                        "T", 
+                        temperature
+                    ]
+                });
             });
-        });
-    }, 1000);
+        }, 1000);
+    });
 };
